10231591844f7a4645b85d95fcfe116acb739476
EkaterinaVishnevskaya <ekaterina2-97@yandex.ru>
2020-07-10 01:16:31 +0300

    Initial Commit

---
+++ UserRegistration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserRegistration
+//
+//  Created by Екатерина Вишневская - ВТБ on 10.07.2020.
+//  Copyright © 2020 Екатерина Вишневская - ВТБ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
